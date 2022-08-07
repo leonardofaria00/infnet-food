b7412003167e90065654d70351a6f652345d3113
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-08-07 15:42:25 -0300

    Added final in Manager Attributes

--- src/main/java/br/infnet/infnetfood/domain/data/model/gerente/Gerente.java
+++ src/main/java/br/infnet/infnetfood/domain/data/model/gerente/Gerente.java
@@ -4,9 +4,9 @@ import br.infnet.infnetfood.domain.data.model.utils.ModelUtils;
 
 public class Gerente extends ModelUtils {
 
-    private Integer matricula;
-    private String nome;
-    private String senha;
+    private final Integer matricula;
+    private final String nome;
+    private final String senha;
 
     public Gerente(final Integer matricula, final String nome, final String senha) {
         this.matricula = matricula;
