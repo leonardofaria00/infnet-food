75b6fa265d1623a12610a8bc273675d4a6786807
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-08-21 16:10:19 -0300

    #Created Delete method

--- src/main/java/br/infnet/infnetfood/controller/GerenteController.java
+++ src/main/java/br/infnet/infnetfood/controller/GerenteController.java
@@ -4,24 +4,41 @@ import br.infnet.infnetfood.domain.data.model.gerente.Gerente;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/food/v1/manager")
 public class GerenteController {
-    public static List<Gerente> lists = new ArrayList<>();
+    public static Map<Integer, Gerente> map = new HashMap<>();
 
     public static void addManager(final Gerente gerente) {
-        lists.add(gerente);
+        map.put(gerente.getMatricula(), gerente);
         gerente.impressao();
+    }
+
+    public static Collection<Gerente> getList() {
+        return map.values();
+    }
+
+    public static void deleteManager(final Integer matricula) {
+        map.remove(matricula);
+        System.out.printf("Deleted registry: %s%n", matricula);
     }
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("listagem", lists);
+        model.addAttribute("listagem", getList());
         return "gerente/lista";
+    }
+
+    @GetMapping("/{matricula}/delete")
+    public String delete(@PathVariable final Integer matricula) {
+        deleteManager(matricula);
+        return "redirect:/food/v1/manager/";
     }
 }
