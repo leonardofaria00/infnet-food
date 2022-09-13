10e116f6f71f7734549789187fc9f026523a5fce
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-09-12 21:55:32 -0300

    #Created Launch read file

--- src/main/java/br/infnet/infnetfood/domain/data/model/refeicao/almoco/AlmocoTest.java
+++ src/main/java/br/infnet/infnetfood/domain/data/model/refeicao/almoco/AlmocoTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 @Order(4)
@@ -41,5 +44,35 @@ class AlmocoTest implements ApplicationRunner {
         a3.setValor(BigDecimal.valueOf(47.00));
         new LaunchController(service).create(a3, "Inclusão de almoço Ratatouille");
         a3.calcularVenda();
+
+        populateFoodWithReadFile();
+    }
+
+    private void populateFoodWithReadFile() {
+        String diretorio = "src/main/resources/";
+        String arquivo = "almoco.txt";
+
+        try {
+            System.out.println("START READ FILE");
+            FileReader fileReader = new FileReader(diretorio + arquivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String row = bufferedReader.readLine();
+            while (row != null) {
+                String[] campos = row.split(";");
+                Almoco almoco = new Almoco();
+                almoco.setNome(campos[0]);
+                almoco.setIngrediente(campos[1]);
+                almoco.setValor(BigDecimal.valueOf(Long.parseLong(campos[2])));
+                new LaunchController(service).create(almoco, "Inclusão via arquivo");
+                row = bufferedReader.readLine();
+            }
+            fileReader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("FINISHED READ FILE!");
+        }
     }
 }
